75b8e9c18e8c13f3e910e2e9d5eda0bff78ce147
Het Mamtora <mamtorahet@yahoo.com>
2025-01-03 18:37:13 +0530

    updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+
+console.log('process.env.MONGODB_URI', process.env.MONGODB_URI);
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGODB_URI), TasksModule],
 })
